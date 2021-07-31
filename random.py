121d6408ba3a3e5ae0521bb294fdf6c2670b1dd9
penguin-number-123 <62600889+penguin-number-123@users.noreply.github.com>
2021-07-31 23:34:13 +0800

    Slow but ok.

--- random.py
+++ random.py
@@ -2,7 +2,8 @@ import math
 import time
 from time import sleep
 print("Random.py 0.0.1")
-amount = int(input("How many random numbers do you want?"))
+f = []
+
 def current_milli_time():
     return round(time.time() * 1000)
 global seed
@@ -11,14 +12,27 @@ def LCG(a):
     seed = a
 def r():
     a = 39081209809
-    b = 2 ** 27
+    b = 2 ** 31
     c = 49094842449
     global seed
     seed = (a*seed + c) % b
     return seed
 def rand():
-    for i in range(amount):
+    for i in range(100):
         LCG(current_milli_time())
-        print(r())
-        sleep(0.001)
-rand()
+        f.append(r())
+        sleep(0.0001)
+def penguinrandom():
+    rand()
+    t = 0
+    s = f[current_milli_time() % 100]
+    t ^= t<< 36
+    t ^= t<<16
+    t ^= s^(s<< 35)
+    return t * current_milli_time()
+g = []
+for i in range(10):            
+    print(penguinrandom())
+    g.append(penguinrandom())
+print(g)
+
