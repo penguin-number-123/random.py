46d981a67ed951855da5458e9fe84d9d10bf4962
penguin-number-123 <62600889+penguin-number-123@users.noreply.github.com>
2021-07-31 20:51:34 +0800

    Update random.py

--- random.py
+++ random.py
@@ -1,8 +1,24 @@
 import math
-print("Random.py 0.0.1 a")
+import time
+from time import sleep
+print("Random.py 0.0.1")
 amount = int(input("How many random numbers do you want?"))
-def lcg(a,b,c):
-    seed = 2**32
-    for i in range(100):
-        seed = (a*seed + c) % b
+def current_milli_time():
+    return round(time.time() * 1000)
+global seed
+def LCG(a):
+    global seed
+    seed = a
+def r():
+    a = 39081209809
+    b = 2 ** 27
+    c = 49094842449
+    global seed
+    seed = (a*seed + c) % b
     return seed
+def rand():
+    for i in range(amount):
+        LCG(current_milli_time())
+        print(r())
+        sleep(0.001)
+rand()
